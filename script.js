799fa22c023eac2f6b5bd238fca96ffb00d2b5ce
Badmintonlegend <sraval2011@gmail.com>
2025-03-16 17:06:00 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var q = 1;
+
+function proccess(a)
+{
+    if(q==1)
+    {
+        if(a==1)
+        {
+            right()
+        }else
+        {
+            wrong()
+        }
+    }
+}
+
+function right()
+{
+    alert("I am happy to hear it")
+}
+function wrong()
+{
+    alert("That's sad")
+}
